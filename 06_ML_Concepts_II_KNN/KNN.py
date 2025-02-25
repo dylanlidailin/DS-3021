ebd5e5fabe876bb8e1cbaa6592de3e9891c650e0
Dailin Li <165076853+dylanlidailin@users.noreply.github.com>
2025-02-25 17:07:51 +0000

    kNN updates

--- 06_ML_Concepts_II_KNN/KNN.py
+++ 06_ML_Concepts_II_KNN/KNN.py
@@ -15,7 +15,6 @@ import random
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.metrics import classification_report
 from sklearn import metrics
-from plot_metric.functions import BinaryClassification #need to pip install plot metric
 
 #%%
 # -------- Data prep --------
